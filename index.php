cc7a7d85edd5ff8f1649b08dde78b09c780ff8b7
Rob Anstee <RAnstee@pressganey.com>
2018-01-19 10:32:54 -0600

    Removed News Feed

--- index.php
+++ index.php
@@ -34,9 +34,7 @@ if ($sess->currsess != false) {
 } else {
     //insecure content here
     ?>
-       <div id="redditnews" style="padding:25px;width:300px;"> 
-          <script src="https://www.reddit.com/hot/.embed?limit=100&t=all" type="text/javascript"></script> 
-       </div>
+     
     <?php include_once("util/login.php");
 }
 ?>
